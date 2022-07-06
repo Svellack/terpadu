45af8502e5e08f22c59425baef1e74283dbfc6c4
luthfynur <nurluthfy98@gmail.com>
2022-07-06 21:43:40 +0700

    fix: Fixing default React testing error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sistem Unit Layanan Terpadu/i);
   expect(linkElement).toBeInTheDocument();
 });
